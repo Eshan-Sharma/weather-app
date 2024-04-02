ae34d06f5bc75e86a3cde6c0dcfcd134744272d9
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-02 14:46:04 +0530

    displayed weather data

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -14,7 +14,7 @@ export default function Weather() {
       const data = await response.json();
       if (data) {
         setLoading(false);
-        console.log(data);
+        setWeatherData(data);
       }
     } catch (error) {
       setLoading(false);
@@ -37,7 +37,38 @@ export default function Weather() {
         search={search}
         setSearch={setSearch}
       />
-      {loading ? <div>Loading please Wait!</div> : null}
+      {loading ? (
+        <div>Loading please Wait!</div>
+      ) : (
+        <div>
+          <div className="city-name mb-4">
+            <h2>
+              {weatherData?.name}, <span>{weatherData?.sys?.country}</span>
+            </h2>
+          </div>
+
+          <div className="temp text-7xl text-black font-bold text-center">
+            {weatherData?.main?.temp}
+          </div>
+          <p className="description text-black text-2xl font-semibold mt-0 mb-8">
+            {weatherData && weatherData.weather && weatherData.weather[0]
+              ? weatherData.weather[0].description
+              : ""}
+          </p>
+          <div className="weather-info flex justify-evenly items-center mt-4 px-5 text-2xl font-bold text-center">
+            <div className="column flex items-center">
+              <div>
+                <p className="wind">{weatherData?.wind?.speed}</p>
+                <p>Wind Speed</p>
+              </div>
+              <div>
+                <p className="humidity">{weatherData?.main?.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
