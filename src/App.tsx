20c5e140b92f0a9c54dfdaca0011be57f95a9744
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-02 11:34:13 +0530

    corrected component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Search from "./components/search/Search";
 import Weather from "./components/weather/Weather";
 
 function App() {
   return (
     <>
-      <Search />
       <Weather />
     </>
   );
