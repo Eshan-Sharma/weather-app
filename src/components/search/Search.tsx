9677612890dc8fc6b0c0578399cb165f8323a694
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-02 11:25:05 +0530

    completed seach component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-
-export default function Search({ search, setSearch }) {
-  const [cityName, setCityName] = useState("");
-  function handleClick() {}
-  //   function handleCityInput(e) {}
+export default function Search({ handleSearch, search, setSearch }) {
   return (
     <div className="w-full flex justify-center items-center mb-8 mt-2">
       <input
-        onChange={(e) => setCityName(e.target.value)}
+        onChange={(e) => setSearch(e.target.value)}
         placeholder="Enter City"
         type="text"
         name="search"
@@ -16,7 +11,7 @@ export default function Search({ search, setSearch }) {
       ></input>
       <button
         className="border-none rounded-lg bg-black text-white text-xl outline-none cursor-pointer px-4 py-2"
-        onClick={handleClick}
+        onClick={handleSearch}
       >
         Search
       </button>
