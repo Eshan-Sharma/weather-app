41747e95646994ad25b8ba94c0a14a8b51d11da0
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-02 09:34:45 +0530

    added tailwind styling

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 
-export default function Search() {
+export default function Search({ search, setSearch }) {
   const [cityName, setCityName] = useState("");
   function handleClick() {}
   //   function handleCityInput(e) {}
   return (
-    <div>
+    <div className="w-full flex justify-center items-center mb-8 mt-2">
       <input
         onChange={(e) => setCityName(e.target.value)}
-        value={cityName}
         placeholder="Enter City"
         type="text"
+        name="search"
+        value={search}
+        className="w-70 h-11 border-2 border-black rounded-lg px-3 text-lg outline-none bg-white text-gray-700 "
       ></input>
-      <button onClick={handleClick}>Search</button>
+      <button
+        className="border-none rounded-lg bg-black text-white text-xl outline-none cursor-pointer px-4 py-2"
+        onClick={handleClick}
+      >
+        Search
+      </button>
     </div>
   );
 }
